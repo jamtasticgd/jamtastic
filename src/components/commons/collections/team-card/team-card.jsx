13c1d2ac2e6376c4aeda65abd31a621e65d1ea9f
Wilkinson Souza <wilki.darolt@gmail.com>
2023-06-15 13:07:04 -0300

    Add team visualization modal.

--- src/components/commons/collections/team-card/team-card.jsx
+++ src/components/commons/collections/team-card/team-card.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 import { Button, Card, Avatar, Tooltip } from '@material-ui/core'
@@ -6,28 +6,38 @@ import RoleIconList from '../role-icon-list/role-icon-list'
 
 import styles from './team-card.module.scss'
 import { useArrayHandler } from 'hooks/use-array-handler'
+import TeamDetails from 'components/pages/team/team-details/team-details'
 
 const TeamCard = ({ data }) => {
+	const [openModal, setOpenModal] = useState(false)
+
+	function onCloseModal() {
+		setOpenModal(false)
+	}
+
 	return (
-		<Card className={styles.container}>
-			<h2>{data.name}</h2>
-			<p>{data.description}</p>
-			<h3>Membros:</h3>
-			<div className={styles.membersRow}>
-				{[...data.members, data.owner].map(member =>
-					<Tooltip key={member.name} title={member.name}>
-						<Avatar src={member.name} alt={member.name} />
-					</Tooltip>
-				)}
-			</div>
-			<h3>Procurando:</h3>
-			<div className={styles.lastLine}>
-				<RoleIconList roles={useArrayHandler(data.needed_skills)} />
-				<div className={styles.btnContainer}>
-					<Button variant="contained" color="secondary">Detalhes</Button>
+		<>
+			<Card className={styles.container}>
+				<h2>{data.name}</h2>
+				<p>{data.description}</p>
+				<h3>Membros:</h3>
+				<div className={styles.membersRow}>
+					{[...data.members, data.owner].map(member =>
+						<Tooltip key={member.name} title={member.name}>
+							<Avatar src={member.name} alt={member.name} />
+						</Tooltip>
+					)}
 				</div>
-			</div>
-		</Card>
+				<h3>Procurando:</h3>
+				<div className={styles.lastLine}>
+					<RoleIconList roles={useArrayHandler(data.needed_skills)} />
+					<div className={styles.btnContainer}>
+						<Button variant="contained" color="secondary" onClick={() => setOpenModal(!openModal)}>Detalhes</Button>
+					</div>
+				</div>
+			</Card>
+			<TeamDetails data={data} open={openModal} onClose={onCloseModal} />
+		</>
 	)
 }
 
